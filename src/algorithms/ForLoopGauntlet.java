ee6d35b4c09017a35e85d9a59eebf07baef2daf9
HelloThereGeneralKenobi <league@WTS5.attlocal.net>
2018-11-04 12:55:49 -0800

    i finished for loop gauntlet and i am going to do some bonus before me tests

--- src/algorithms/ForLoopGauntlet.java
+++ src/algorithms/ForLoopGauntlet.java
@@ -38,10 +38,39 @@ public class ForLoopGauntlet {
 				}
 				
 			}
-		for(int i = 2007; i <2018; i++) {
-			System.out.println(i+"i was "+i);
+		for(int i = 2007; i <2019; i++) {
+			System.out.println("In the year "+i+" I was "+(i-2007));
 			
 		}
-	
+	for(int i = 0; i <3; i++) {
+		for (int j = 0; j <3; j++) {
+			System.out.println(i+" "+j);
+		}
+		
+	}
+	for(int i =0;i<9;i+=3) {
+		for(int k =1; k<4;k++) {
+			System.out.print(k+i+" ");
+			
+		}
+		System.out.println();
+	}
+	for(int i =0;i<100;i+=10) {
+		for(int k =1; k<11;k++) {
+			System.out.print(k+i+" ");
+			
+		}
+		System.out.println();
+	}
+	for(int i =1;i<7;i++) {
+		for(int k =0; k<i;k++) {
+			System.out.print(" *");
+			
+		}
+		System.out.println();
+	}
+	for(String s = "";s.length()<14;s+=" *") {
+		System.out.println(s);
+	}
 }
 }

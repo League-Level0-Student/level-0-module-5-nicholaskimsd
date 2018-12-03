7ec832f3da28e4e6ecc565520723292abca62f84
nicholaskimsd <league@WTS-IM-17.attlocal.net>
2018-12-02 17:23:40 -0800

    i did more practice tests

---
+++ src/nested_loops/Joid.java
@@ -0,0 +1,17 @@
+package nested_loops;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Joid {
+public static void main(String[] args) {
+	Robot rob = new Robot();
+	rob.penDown();
+	rob.setSpeed(200);
+	rob.rect(0, 0, 200, 200);
+	}
+
+
+	
+}
+
+
